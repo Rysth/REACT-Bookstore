1bf27838dbb4c3c6d7dfc0111d5906df6aad3cdd
Rysth <johnpalacios.t@gmail.com>
2023-08-24 14:56:34 -0500

    Finished the Navigation desktop version.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,23 +5,21 @@ import './Navigation.css';
 function Navigation() {
   return (
     <div className="header container">
-      <div className="header-left">
+      <nav className="header-left">
         <h1 className="header-title">Bookstore CMS</h1>
-        <nav className="header-nav">
-          <ul className="header-list">
-            <li className="header-item">
-              <Link to="/" className="header-link active">
-                Books
-              </Link>
-            </li>
-            <li className="header-item">
-              <Link to="/categories" className="header-link">
-                Categories
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+        <ul className="header-list">
+          <li className="header-item">
+            <Link to="/" className="header-link active">
+              Books
+            </Link>
+          </li>
+          <li className="header-item">
+            <Link to="/categories" className="header-link">
+              Categories
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <div className="header-right">
         <div className="header-user">
           <button className="header-button" type="button">
